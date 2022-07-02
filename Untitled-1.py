e88de9f837db3346f9ca86f1a0f970f40b536d1d
sakshamp10 <saksham21486@iiitd.ac.in>
2022-07-02 12:56:38 +0530

    update 2ND JULY

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,4 +1,6 @@
 r0 = r1 = r2 = r3 = r4 = r5 = r6 = flag = 0
+var_list = {}
+labels = {}
 
 registers = {
     "r0": "000",
@@ -10,28 +12,6 @@ registers = {
     "r6": "110",
     "flags": "111",
 }
-var_list = {}
-# opcode={
-#     "add":"10000",
-#     "sub":"10001",
-#     "mov":["10010","10011"],
-#     "ld":"10100",
-#     "st":"10101",
-#     "mul":"10110",
-#     "div":"10111",
-#     "rs":"11000",
-#     "ls":"11001",
-#     "xor":"11010",
-#     "or":"11011",
-#     "and":"11100",
-#     "not":"11101",
-#     "cmp":"11110",
-#     "jmp":"11111",
-#     "jlt":"01100",
-#     "jgt":"01101",
-#     "je":"01111",
-#     "hlt":"01010"
-# }
 
 A = {
     "add": "10000",
@@ -76,39 +56,52 @@ def dtob(dec):
     while (dec > 0):
         s = (str)(dec % 2) + s
         dec = dec // 2
+    if (len(s) < 8):
+        s = (8 - len(s)) * "0" + s
     return s
 
 
 def errorHandle(i):
     print(f"INVALID OPERATION AT LINE {i}")
+    exit()
 
 
 def ins_valid(s, i):
     if (s[0] in A):
         if (len(s) != 4):
             print(f"INVALID INSTRUCTION AT LINE {i}")
+            exit()
         if (s[1] not in registers or s[2] not in registers or s[3] not in registers):
             print(f"INVALID REGISTER NAME USED IN LINE {i}")
+            exit()
+
+    #length of instruction to be checked for all the below instruction types
 
     if (s[0] in B):
         if (s[1] not in registers):
             print(f"INVALID REGISTER NAME USED IN LINE {i}")
+            exit()
         if ("$" not in s[2]):
             print(f"INVALID INSTRUCTION AT LINE {i}")
+            exit()
         if (int(s[2][1:]) > 127):
             print(f"VALUE OF IMMEDIATE EXCEEDS THE LIMIT(MORE THAN 8 BITS) AT LINE {i}")
-
+            exit()
     if (s[0] in C):
         if (s[1] not in registers or s[2] not in registers):
             print(f"INVALID REGISTER NAME USED IN LINE {i}")
+            exit()
     if (s[0] in D):
         if (s[1] not in registers):
             print(f"INVALID REGISTER NAME USED IN LINE {i}")
+            exit()
         if (s[2] not in var_list):
             print(f"INVALID MEMORY ADDRESS AT LINE {i}")
+            exit()
     if (s[0] in E):
         if (s[1] not in var_list):
             print(f"INVALID MEMORY ADDRESS AT LINE {i}")
+            exit()
 
 
 def typeA(s):
@@ -120,28 +113,28 @@ def typeA(s):
 
 def typeB(s):
     ans = ""
-    ans = B[s[0]] + registers(s[1]) + dtob(s[2][1:])
+    ans = B[s[0]] + registers[s[1]] + dtob(s[2][1:])
     f = open("binary.txt", "a")
     f.write(ans + "\n")
 
 
 def typeC(s):
     ans = ""
-    ans = C[s[0]] + "00000" + registers[s[1]] + registers[s[2]]
+    ans = C[s[0]] + "0" * 5 + registers[s[1]] + registers[s[2]]
     f = open("binary.txt", "a")
     f.write(ans + "\n")
 
 
 def typeD(s):
     ans = ""
-    ans = D[s[0]] + registers[s[1]] + var_list[]  # memadress to be added
+    ans = D[s[0]] + registers[s[1]] + var_list[s[2]][0]
     f = open("binary.txt", "a")
     f.write(ans + "\n")
 
 
 def typeE(s):
     ans = ""
-    ans = E[s[0]] + "000"  # memadress to be added
+    ans = E[s[0]] + "000"           # memadress of label to be added
     f = open("binary.txt", "a")
     f.write(ans + "\n")
 
@@ -153,33 +146,36 @@ def typeF(s):
     f.write(ans + "\n")
 
 
-def errorHandle():
-    pass
-
-
 with open("file.txt", "r") as f:
     l = []
     var_count = 0
-    ins_count = 0
+    line_count = 0
     for i in f:
         s = i.split()
         l.append(s)
-        ins_count += 1
+        line_count += 1
 
         if (s[0] == "var"):
             if (len(var_list) > 2 ** 8):
-                print("TOO MANY VARIBALES")
+                print("TOO MANY VARIABLES")
             else:
-                var_list[s[1]] = [0, 0]
+                var_list[s[1]] = ["0", 0]  # [address,value] of variable
                 var_count += 1
 
-    curr_address = ins_count + 1
+    curr_address = line_count - var_count
     for i in var_list:
-        var_list[i][0] = curr_address
+        d = dtob(curr_address)
+        if (len(d) > 8):
+            print("INVALID MEMORY ADDRESS FOUND")
+            exit()
+        var_list[i][0] = d
         curr_address += 1
 
     i = 1
     for s in l:
+
+        #variables labels to be handled here
+
         if (s[0] == "mov"):
             if ("$" in s[2]):
                 typeB(s)
