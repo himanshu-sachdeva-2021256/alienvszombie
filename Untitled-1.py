a093cc004fb2eb3b436b037ee5e9100b8eaea873
himanshu-sachdeva-2021256 <himanshu21256@iiitd.ac.in>
2022-07-03 19:27:26 +0530

    clear file content

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,14 +3,14 @@ var_list = {}
 labels = {}
 
 registers = {
-    "r0": "000",
-    "r1": "001",
-    "r2": "010",
-    "r3": "011",
-    "r4": "100",
-    "r5": "101",
-    "r6": "110",
-    "flags": "111",
+    "R0": "000",
+    "R1": "001",
+    "R2": "010",
+    "R3": "011",
+    "R4": "100",
+    "R5": "101",
+    "R6": "110",
+    "FLAGS": "111",
 }
 
 A = {
@@ -156,6 +156,8 @@ def typeF(s,i):
 variable_flag_check = 0
 
 with open("file.txt", "r") as f:
+    g=open("binary.txt","w")
+    g.close()
     l = []
     var_count = 0
     line_count = 0
